f4e87ec459f0b6607cbb484f1e928c8a4a779c1f
juliaszbrt <112975933+juliaszbrt@users.noreply.github.com>
2025-02-01 22:17:34 -0500

    Fix some formatting

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 import streamlit as st
 import pandas as pd
+import requests
+from config import GOOGLE_MAPS_API_KEY
 
 st.set_page_config(layout="wide")
 
-# Sidebar for dietary restrictions
-st.sidebar.header("Select Your Dietary Preferences")
-with st.sidebar.expander("Dietary Preferences", expanded=True):
-    dietary_preferences = {
-        "Vegan": st.sidebar.checkbox("Vegan"),
-        "Lactose-Free": st.sidebar.checkbox("Lactose-Free"),
-        "Gluten-Free": st.sidebar.checkbox("Gluten-Free"),
-        "Halal": st.sidebar.checkbox("Halal")
-    }
+col1, col2 = st.columns([2, 5])
 
-selected_preferences = [key for key, value in dietary_preferences.items() if value]
+with col1:
 
-def display_map():
+    st.header("Select Your Dietary Preferences")
+    
+    halal = st.checkbox("Halal")
+    vegan = st.checkbox("Vegan")
+
+    selected_restrictions = []
+    if vegan:
+        selected_restrictions.append("Vegan")
+    if halal:
+        selected_restrictions.append("Halal")
+
+with col2:
     # Data frame with location of Quebec City
     data = pd.DataFrame({
         'lat': [45.5017],
@@ -23,7 +28,3 @@ def display_map():
     })
 
     st.map(data)
-
-
-
-display_map()
